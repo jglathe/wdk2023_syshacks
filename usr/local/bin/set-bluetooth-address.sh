08eaa21b02a2798640b02fe3f8d694bfbfc61855
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2023-10-24 20:05:00 +0200

    add 120s timeout if hci0 fails to come up
    Signed-off-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>

--- usr/local/bin/set-bluetooth-address.sh
+++ usr/local/bin/set-bluetooth-address.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Wait for hci0 to be up
-while [[ ! -d "/sys/class/bluetooth/hci0" ]]; do
-	sleep 1
+counter=0
+while [[ ! -d "/sys/class/bluetooth/hci0" ]] && [[ $counter -lt 120 ]]; do
+        sleep 5
+        let counter=counter+5
 done
 
-#wait a few seconds more to be sure
-sleep 5
+if [[ $counter -eq 120 ]]; then
+        echo "Error: Device not found within the specified time limit"
+        exit 1
+fi
 
 # Now that hci0 is up, set the MAC address
 /usr/bin/btmgmt public-addr AD:5A:00:F0:FD:8C
